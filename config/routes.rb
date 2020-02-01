68b45d0a09bec7afd73d88c5ccce8169f673a886
kana0313 <kokusitaisaku.kana@gmail.com>
2020-02-01 16:31:33 +0900

    投稿機能実装済み

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root  'hyousis#index' 
   get 'tweets' => 'tweets#index'
   get  'tweets/new'  =>  'tweets#new'   
+  post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
 end
