6a2dc0c59c6189521527365fed8034e1c013f340
kana0313 <kokusitaisaku.kana@gmail.com>
2020-02-01 18:27:29 +0900

    投稿実装（画像未）

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root  'hyousis#index' 
   get 'tweets' => 'tweets#index'
   get  'tweets/new'  =>  'tweets#new'   
   post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
+  get   'users/:id'   =>  'users#show'    #Mypageへのルーティング
+ 
+   
 end
